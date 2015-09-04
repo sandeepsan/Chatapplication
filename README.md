5f2e1924f900dc25b862ab0007b32a98ab324567
sandeep <sandyenc@gmail.com>
2015-09-04 09:25:51 -0700

    Initial version of chat application

---
+++ README.md
@@ -0,0 +1,10 @@
+# ChatappFse
+Chat application for FSE
+1. Prerequisites for running: 
+   Install Node js, redis, socket.io in the test system
+2. Run chat server using "node chatapp.js"
+3. Go to browser and use 3000 port, localhost for testing chat application by typing "http://localhost:3000"
+4. Enter the name of client and start chatting. If the name is not given, it will prompt user until proper name is entered.
+5. To have another client, open another tab and type "http://localhost:3000"
+6. Enter the new client name and start chatting. 
+7. Redis is used for maintaining database, so all previous message history will be available
---
+++ redis/chat-app.js
@@ -0,0 +1,64 @@
+var app = require("http").createServer(handler),
+    io = require("socket.io").listen(app),
+    fs = require("fs");
+    redis = require("redis");
+    client = redis.createClient();
+
+app.listen(3000);
+
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+//var messages=[];
+var storeMessage=function(data){
+
+//messages.push({data:data});
+//if(messages.length > 1000)
+//{
+//	messages.shift();
+//}
+var message = JSON.stringify({data:data});
+client.lpush("messages",message,function(err, response){
+client.ltrim("messages",0,9);
+});
+}
+
+io.sockets.on('connection', function (socket) {
+    //when recieving the data from the server, push the same message to client.
+    socket.on("clientMsg", function (data) {
+        //send the data to the current client requested/sent.
+        socket.emit("serverMsg", data);
+        //send the data to all the clients who are acessing the same site(localhost)
+        socket.broadcast.emit("serverMsg", data);
+	storeMessage(data);
+    });
+
+    socket.on("sender", function (data) {
+        socket.emit("sender", data);
+        socket.broadcast.emit("sender", data);
+    });
+
+    socket.on("join", function(data){
+        client.lrange("messages",0,-1,function(err, messages){
+        messages=messages.reverse();
+        
+        messages.forEach(function(message){
+        message = JSON.parse(message);
+	socket.emit("serverMsg", message.data);
+       });
+       });
+    });
+        
+});
+
+
+
